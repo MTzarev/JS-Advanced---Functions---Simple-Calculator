c515330bcd8538e898b957371690e6f054004245
Miroslav Tsarev <mtzarev@gmail.com>
2024-05-30 21:32:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function calculate() {
+    let firstElement;
+    let secondElement;
+    let resultElement;
+    return {
+        init: (selector1, selector2, resultSelector)=>{
+            firstElement = document.querySelector(selector1);
+            secondElement = document.querySelector(selector2);
+            resultElement = document.querySelector(resultSelector)
+        },
+        add: ()=>{
+            resultElement.value=Number(firstElement.value)+Number(secondElement.value)
+        },
+        subtract: ()=>{
+            resultElement.value=Number(firstElement.value)-Number(secondElement.value)
+
+        }
+     }
+}
+let obj = calculate();
+obj.init(`#num1`, `#num2`, `#result`)
+
+let add = obj.add;
+let subtract=obj.subtract;
+
+
+
